ba6dc1b5631ec8b597e5e2bf9bcda7c508a9b053
ferdyansahalfariz <96871156+ferdyansahalfariz@users.noreply.github.com>
2024-01-20 21:48:25 +0700

    Update TravelidApplication.java

--- src/main/java/com/backend/travelid/TravelidApplication.java
+++ src/main/java/com/backend/travelid/TravelidApplication.java
@@ -5,16 +5,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
 @OpenAPIDefinition
 public class TravelidApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TravelidApplication.class, args);
 	}
-
 }
